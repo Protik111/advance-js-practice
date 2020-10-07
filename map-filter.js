a2be6d507f8a129bac93b7d5dde96163e650f4ab
Protik111 <rafiurprotik111.gmail.com>
2020-10-07 22:31:32 +0600

    upto object property

---
+++ map-filter.js
@@ -0,0 +1,25 @@
+const numbers = [5, 6, 8, 9, 10];
+
+//way by for loop
+
+const numbersDouble = [];
+for(let i=0; i<numbers.length;i++){
+    const element = numbers[i];
+    const result = element*element;
+    numbersDouble.push(result);
+}
+console.log(numbersDouble);
+
+//way by map
+
+const age = [20, 15, 21, 22, 4, 2];
+const result = age.map(function(elements){
+    // console.log(elements*elements); //this line gives the doubled value individually
+    return elements*elements;
+})
+
+const bigger = age.filter(x=> x>5);
+console.log(bigger);
+
+// const result2 = numbers.map(elements=> elements*elements); //shortcut way by arrow function
+console.log(result);
