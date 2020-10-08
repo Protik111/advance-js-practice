4feebac3b8d5b0fdff449c1540d23eaee1de35a6
Protik111 <rafiurprotik111.gmail.com>
2020-10-08 09:12:21 +0600

    done successfully

--- map-filter.js
+++ map-filter.js
@@ -17,6 +17,7 @@ const result = age.map(function(elements){
     // console.log(elements*elements); //this line gives the doubled value individually
     return elements*elements;
 })
+console.log(result);
 
 const bigger = age.filter(x=> x>5);
 console.log(bigger);
